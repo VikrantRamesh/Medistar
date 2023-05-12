2efa7668d24f2ca9a23d91b0435db4dcfc1bb575
VikrantRamesh <94131187+VikrantRamesh@users.noreply.github.com>
2023-05-12 20:11:28 +0530

    Uploading server Files version - 2.0.0

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 var db_control = require.main.require("./models/db_controller");
 const cookieParser = require("cookie-parser");
+const { Console } = require("console");
 
 
 //var router = express.Router();
@@ -24,7 +25,7 @@ app.use(cors({
     origin: 'http://localhost:3000',
     credentials: true
   }));
-  
+
 
 const db = mysql.createConnection({
     use: "root",
@@ -75,8 +76,8 @@ app.post("/doctor_signup", async (req, res) =>{
             req.body.email,
             req.body.password,
             req.body.class
-        ).then(() => process.exit());  
-    
+        ).then(() => process.exit());
+
     //console.log(req.body.dob.slice(0,4), primaryKey);
     const docAge = 2023 - req.body.dob.slice(0,4);
 
@@ -84,12 +85,12 @@ app.post("/doctor_signup", async (req, res) =>{
         primaryKey,
         req.body.fname,
         req.body.lname,
-        req.body.email, 
+        req.body.email,
         req.body.dob,
         docAge,
         req.body.gender,
         req.body.address,
-        req.body.phone, 
+        req.body.phone,
         req.body.qual,
         req.body.dept
     );
@@ -106,7 +107,7 @@ app.post("/admin_signup", async (req, res) =>{
             req.body.email,
             req.body.password,
             req.body.class
-        ).then(() => process.exit());  
+        ).then(() => process.exit());
 
     db_control.admin_signup(
         primaryKey
@@ -115,7 +116,7 @@ app.post("/admin_signup", async (req, res) =>{
 
 
 app.post("/login", async (req, res) =>{
-    
+
     con.query('select * from users where username = ? and password = ?' , [req.body.username, req.body.password], function(error , results , fields){
         if(results){
             if (results.length > 0){
@@ -149,7 +150,7 @@ app.post("/login", async (req, res) =>{
 
 app.post("/get_patient_id", async (req, res) =>{
     con.query('select p.pid , p.p_fname from patient p inner join users u on p.user_id = u.id where u.id = ?' , [req.body.userId], function(error , results , fields){
-        
+
         if(results){
             if (results.length > 0){
               //console.log(results[0].pid);
@@ -179,7 +180,7 @@ app.post("/get_patient_id", async (req, res) =>{
 });
 
 app.post("/get_doctor_id", async (req, res) =>{
-    
+
     con.query('select p.doc_id , p.first_name from doctor p inner join users u on p.user_id = u.id where u.id = ?' , [req.body.userId], function(error , results , fields){
         if(results){
             if (results.length > 0){
@@ -195,7 +196,7 @@ app.post("/get_doctor_id", async (req, res) =>{
 });
 
 app.post("/get_admin_id", async (req, res) =>{
-    
+
     con.query('select a.admin_id from admin a where a.user_id = ?' , [req.body.userId], function(error , results , fields){
         if(results){
             if (results.length > 0){
@@ -213,10 +214,10 @@ app.post("/get_admin_id", async (req, res) =>{
 
 app.post("/get_doc_details", async (req, res) =>{
     con.query('select * from doctor d where d.doc_id = ?' , [req.body.doc_id], function(error , results , fields){
-        
+
         if(results){
             if (results.length > 0){
-                
+
                 const fname = results[0].first_name;
                 const lname = results[0].last_name;
                 const email = results[0].email;
@@ -250,10 +251,10 @@ app.post("/get_doc_details", async (req, res) =>{
 
 app.post("/get_admin_details", async (req, res) =>{
     con.query('select * from users u where u.id = ?' , [req.body.user_id], function(error , results , fields){
-        
+
         if(results){
             if (results.length > 0){
-                
+
                 const user_name = results[0].username;
                 const email = results[0].email;
 
@@ -279,7 +280,7 @@ const authorization = (req, res, next) => {
     }
     try {
       const data = jwt.verify(token, "YOUR_SECRET_KEY");
-      
+
       req.userId = data.id;
       req.userClass = data.class;
       return next();
@@ -310,7 +311,7 @@ app.post("/get_symptoms", async (req, res) =>{
     });
     res.json({symptoms: symptom});
   });
-    
+
 });
 
 app.post("/get_disease_symptoms", async (req, res) =>{
@@ -341,9 +342,10 @@ app.post("/get_appointments", async (req, res) =>{
     let year = date_ob.getFullYear();
 
     const app_date = year + "-" + month + "-" + date;
+
     con.query('select * from appointment where doc_id = ? and date = ?',[req.body.doc_id,app_date], function(error , results , fields){
         const Appointments = [];
-        
+
         results.forEach((row) => {
             const app = {}
             const appno = row.id;
@@ -366,9 +368,8 @@ app.post("/get_appointments", async (req, res) =>{
 });
 
 app.post("/get_doctors", async (req, res) =>{
-    //console.log(req.body.disease);
+
     con.query('SELECT d.doc_id , d.first_name, d.last_name, d.department, d.qualification from doctor d inner join departments dp on d.department = dp.department_name inner join disease_department dd  on dp.id = dd.department_id inner join diseases di on di.disease_id = dd.disease_id Where di.name = ?', [req.body.disease], function(error , results , fields){
-        
         const doc = [];
 
         results.forEach((row) => {
@@ -386,15 +387,33 @@ app.post("/get_doctors", async (req, res) =>{
             doctors["department"] = (department);
             doc.push(doctors);
         });
-        //console.log(doctors);
+
         res.json({doctors: doc});
+    });
+});
+
+
+app.post("/get_doctor_appointments", async (req, res) =>{
+
+    con.query(`
+    SELECT COUNT(*) as app_count
+    FROM appointment 
+    WHERE doc_id = ?
+    AND time = ? 
+    AND date = ?;
+    `,[req.body.doc_id,req.body.time,req.body.date], function(error , results , fields){
+        if(results){
+            if (results.length > 0){
+                res.json({count: results[0].app_count});
+            }
+        }       
     });
 });
 
 app.post("/get_doctors_dept", async (req, res) =>{
     //console.log(req.body.disease);
     con.query('SELECT d.doc_id , d.first_name, d.last_name, d.department, d.qualification from doctor d inner join departments dp on d.department = dp.department_name where dp.department_name = ?', [req.body.dept], function(error , results , fields){
-        
+
         const doc = [];
 
         results.forEach((row) => {
@@ -422,12 +441,12 @@ app.post('/get_images', (req, res) => {
         con.query(`SELECT img FROM images WHERE id = ?`, [req.body.user_id], function(error , results , fields){
             if(results.length > 0){
                 res.set('Content-Type', 'image/jpg')
-                    .send(results[0].img); 
+                    .send(results[0].img);
                 }else{
                     res.send();
                 }
         });
-  });
+});
 
 
 
@@ -443,7 +462,7 @@ app.post("/detect_disease", async(req, res)=>{
         if (err) throw err;
         if (results.length > 0) {
             // Return array of possible diseases
-            
+
             const disease = [];
             results.forEach((row) => {
                 disease.push(row.name)
@@ -451,9 +470,9 @@ app.post("/detect_disease", async(req, res)=>{
             const die = disease[0];
             console.log(`Possible diseases: ${disease}`);
             res.cookie("disease",die);
-            
+
             //console.log("cookie set",res.cookies);
-            
+
             res.json({diseases:disease});
         } else {
             res.cookie("disease",[]);
@@ -489,7 +508,7 @@ app.post("/appointment", async(req, res)=>{
 });
 
 app.post("/insert_prescription", async(req, res)=>{
-    let query = "INSERT INTO `prescriptions`(`app_id`,`pid`, `doc_id`,`doc_name`,`pname`,`date`,`time`,`prescription`) VALUES (" +
+    let query = "INSERT INTO `prescriptions`(`app_id`,`pid`, `doc_id`,`doc_name`,`pname`,`date`,`time`,`prescription`, `disease`) VALUES (" +
     req.body.appno +
     "," +
     req.body.pid +
@@ -505,6 +524,8 @@ app.post("/insert_prescription", async(req, res)=>{
     req.body.time +
     "','" +
     req.body.pres +
+    "','" +
+    req.body.disease +
     "')";
     console.log(query);
     con.query(query, (err, results) => {
@@ -512,9 +533,211 @@ app.post("/insert_prescription", async(req, res)=>{
             res.send();
         }) ;
     });
-    
+
 });
 
+
+
+
+
+
+// ---ADMIN DASHBOARD QUERIES---
+
+app.post("/get_website_stats", async (req, res) =>{
+    //console.log(req.body.disease);
+    con.query('SELECT count(*) as cnt from users u group by user_class order by user_class', function(error , results , fields){
+        
+        if(results){
+            if (results.length > 0){
+                //console.log(doctors);
+                res.json({admins: results[0]["cnt"],
+                        doctors: results[1]["cnt"],
+                        patients: results[2]["cnt"],
+                        total: results[0]["cnt"]+results[1]["cnt"]+results[2]["cnt"]});
+            }
+        }
+    });
+});
+
+app.post("/dept_appointment_data", async (req, res) =>{
+    //console.log(req.body.disease);
+
+    
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+
+    const app_date = year + "-" + month + "-" + date;
+
+    con.query('SELECT department,count(*) as dept_cnt from appointment where date = ? group by department;',[app_date], function(error , results , fields){
+        app_data = {}
+        if(results){
+            if (results.length > 0){
+
+                results.forEach((row)=>{
+                    app_data[row.department] = row.dept_cnt;
+                });
+            }
+        }
+        return res.json(app_data);
+    });
+});
+
+app.post("/time_appointment_data", async (req, res) =>{
+    //console.log(req.body.disease);
+
+    
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+
+    const app_date = year + "-" + month + "-" + date;
+
+    con.query('SELECT time,count(*) as time_cnt from appointment where date = ? group by time order by time',[app_date], function(error , results , fields){
+        
+        if(results){
+            if (results.length > 0){
+                app_data = {"00:00":0, "10:00 AM":0, "11:00 AM":0,"12:00 PM":0,"01:00 PM":0,"02:00 PM":0,"03:00 PM":0,"04:00 PM":0,"05:00 PM":0}
+                results.forEach((row)=>{
+                    app_data[row.time] = row.time_cnt;
+                });
+            }
+        }       
+        return res.json(app_data);
+    });
+});
+
+app.post("/gender_data", async (req, res) =>{
+
+
+
+    con.query('SELECT gender,count(*) as gender_cnt from patient group by gender', function(error , results , fields){
+        let total = 0;
+        let app_data = {"Male":0, "Female":0, "Other":0};
+        if(results){
+            if (results.length > 0){
+                
+                results.forEach((row)=>{
+                    total+=row.gender_cnt;
+                    app_data[row.gender] = row.gender_cnt;
+                });
+            }
+        }       
+        app_data["total"] = total;
+        return res.json(app_data);
+    });
+});
+
+
+app.post("/age_data", async (req, res) =>{
+    //console.log(req.body.disease);
+    con.query(`
+    SELECT 
+        CASE 
+            WHEN age < 10 THEN '0-10' 
+            WHEN age BETWEEN 10 AND 20 THEN '10-20' 
+            WHEN age BETWEEN 20 AND 30 THEN '20-30' 
+            WHEN age BETWEEN 30 AND 40 THEN '30-40' 
+            WHEN age BETWEEN 40 AND 50 THEN '40-50' 
+            WHEN age BETWEEN 50 AND 60 THEN '50-60' 
+            WHEN age BETWEEN 60 AND 70 THEN '60-70' 
+            WHEN age BETWEEN 70 AND 80 THEN '70-80' 
+            WHEN age BETWEEN 80 AND 90 THEN '80-90' 
+            WHEN age BETWEEN 90 AND 100 THEN '90-100' 
+            
+            ELSE 'Over 100' 
+        END AS age_group, 
+        COUNT(*) AS num_people 
+        FROM patient 
+        GROUP BY age_group;
+    `, function(error , results , fields){
+        let app_data = {"0-10":0, "10-20":0, "20-30":0, "30-40":0,"40-50":0, "50-60":0,"60-70":0, "70-80":0,"80-90":0, "90-100":0,"100+":0};
+        if(results){
+            if (results.length > 0){
+                
+                results.forEach((row)=>{
+                    app_data[row.age_group] = row.num_people;
+                });
+            }
+        }       
+        return res.json(app_data);
+    });
+});
+
+
+app.post("/top_docs", async (req, res) =>{
+    //console.log(req.body.disease);
+
+    
+    let date_ob = new Date();
+    let date = ("0" + date_ob.getDate()).slice(-2);
+    let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+    let year = date_ob.getFullYear();
+
+    const app_date = year + "-" + month + "-" + date;
+
+    con.query(`
+        SELECT d.doc_id , d.first_name, d.last_name, d.department, d.qualification, COUNT(*) AS num_appointments
+        FROM appointment p
+        INNER JOIN doctor d ON p.doc_id = d.doc_id
+        WHERE date = ?
+        GROUP BY p.doc_id
+        ORDER BY num_appointments DESC
+        LIMIT 3;
+    `,[app_date], function(error , results , fields){
+        console.log(results);
+        if(results){
+            if (results.length > 0){
+                const doc = [];
+                results.forEach((row) => {
+                    const doctors = {}
+                    const id = row.doc_id;
+                    const fname = row.first_name;
+                    const lname = row.last_name;
+                    const qualification = row.qualification;
+                    const department = row.department;
+                    const num_appointments =row.num_appointments
+
+                    doctors["id"] =(id);
+                    doctors["fname"] =(fname);
+                    doctors["lname"] = (lname);
+                    doctors["qualification"] =(qualification);
+                    doctors["department"] = (department);
+                    doctors["num_appointments"] = (num_appointments);
+                    doc.push(doctors);
+                });
+                //console.log(doctors);
+                res.json({doctors: doc});
+            }
+        }       
+    });
+});
+
+
+app.post("/top_diseases", async (req, res) =>{
+    //console.log(req.body.disease);    
+
+    con.query(`
+    select disease,COUNT(*) AS total_cases from prescriptions group by disease order by total_cases desc LIMIT 5;
+    `, function(error , results , fields){
+        if(results){
+            if (results.length > 0){
+                const diseases = {};
+                results.forEach((row) => {
+                    const disease = row.disease;
+                    const total_cases = row.total_cases;
+
+                    diseases[disease] =(total_cases);
+                });
+                //console.log(doctors);
+                
+                res.json({diseases: diseases});
+            }
+        }       
+    });
+});
 
 
 
